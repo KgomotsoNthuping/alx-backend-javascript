de8d963b5630a4083f1ee4e46402b02d7c476671
KgomotsoNthuping <kgomotsonthuping22@gmail.com>
2024-05-29 20:18:03 +0000

    es6

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,8 @@
+
+rt default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(console.log('Got a response from the API'));
+}
+
